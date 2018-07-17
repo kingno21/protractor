31024dd204651d0615f705e538c7e88e8ed9ebef
kingno21 <kingno21@gmail.com>
2018-07-17 17:58:26 +0900

    test

--- index_spec.js
+++ index_spec.js
@@ -1,7 +1,8 @@
 describe('Protractor Demo App', function() {
   it('should have a title', function() {
-    browser.get('http://juliemr.github.io/protractor-demo/');
+    browser.ignoreSynchronization = true
+    browser.get('https://www.google.co.jp');
 
-    expect(browser.getTitle()).toEqual('Super Calculator');
+    expect(browser.getTitle()).toEqual('Google');
   });
 });
